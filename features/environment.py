8a2d45906017f6c76e255a10b1b1a23d2736cf28
MichaelWittgreffe <m.wittgreffe@btopenworld.com>
2020-09-27 22:21:27 +0100

    added initial tests

--- features/environment.py
+++ features/environment.py
@@ -12,16 +12,26 @@ from generic_api.template_constants import template_constants
 @fixture
 def populate_template_constants(context: Context) -> bool:
     "adds the template constants to the context"
+    if context is None:
+        raise RuntimeError("Context Is None")
+
     context.templates = template_constants
     return True
 
 
 def after_scenario(context: Context, scenario: Scenario):
+    "runs after a scenario has finished, used to reset values"
+    if context is None:
+        raise RuntimeError("Context Is None")
+
     context.templates = {}
     context.default_values = {}
 
 
 def before_tag(context: Context, tag: Tag):
+    if context is None or tag is None:
+        raise RuntimeError("Context Or Tag Is None")
+
     if tag.startswith("constants"):
         return use_fixture_by_tag(tag, context, fixture_registry)
 
--- generic_api/template_constants.py
+++ generic_api/template_constants.py
@@ -2,6 +2,4 @@
 # this is a dictionary of request templates that won't change, we can hold them as constants instead of defining
 # them in the behave file every single time we want to make the request
 
-template_constants: dict = {
-
-}
+template_constants: dict = {}
